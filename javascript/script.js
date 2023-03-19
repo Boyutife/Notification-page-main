c3754b81473ce64fe93536451aab914fbba8bdfa
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2023-03-19 14:50:26 +0100

    improved the code

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,50 @@
-const notifications = document.querySelectorAll('li'),
- readAll = document.querySelector('.read_all'),
- dots = document.querySelectorAll('span.dot'),
- num = document.querySelector('.num'),
- privateMessage = document.querySelector('.private_message');
+//  select DOM elements
+
+const notificationsList = document.querySelectorAll('li'),
+ readAllButton = document.querySelector('.read_all'),
+ dotsList = document.querySelectorAll('span.dot'),
+ notificationCount = document.querySelector('.notification_count'),
+ privateMessageModal = document.querySelector('.private_message_modal');
 
 
+// function to handle notification click event
 
-notifications.forEach((notification, index) => {
-  notification.addEventListener('click', () => {
-  if(notification.classList.contains('message')){ privateMessage.style.display = 'block'};
+function handleNotificationClick(notification, index){
+  // show private message modal if notification is a message 
+  if(notification.classList.contains('message')){
+    privateMessageModal.style.display = 'block';
+  }
+  // mark notification as read
   notification.classList.remove('unread');
-   dots[index].classList.remove('dot');
-   num.innerText = document.querySelectorAll('li.unread').length;
+  dotsList[index].classList.remove('dot');
+  // update notififications count
+  const unreadNotifications = document.querySelectorAll('li.unread');
+  notificationCount.innerText = unreadNotifications.length;
+};
+
+// add click event listener to each notification
+for (let i = 0; i < notificationsList.length; i++) {
+  const notification = notificationsList[i];
+  notification.addEventListener('click', () => handleNotificationClick(notification, i));
+}
+
+// add click event listener to "mark all as read" button
+readAllButton.addEventListener('click', () => {
+  // mark all notifications as read
+  notificationsList.forEach(notification => {
+    if (notification.classList.contains('message')) {
+      privateMessageModal.style.display = 'block';
+    }
+    notification.classList.remove('unread');
   });
+  dotsList.forEach(dot => dot.classList.remove('dot'));
+  // update notification count
+  notificationCount.innerText = 0;
 });
+
+
+
+
 
  
 
